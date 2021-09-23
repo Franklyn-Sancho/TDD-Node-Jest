e006cf36ee67234fac0ff8599ed20e8e76e84d8e
Franklyn Sancho <franklyn.hs@gmail.com>
2021-09-23 13:46:28 -0300

    Teste de URL com Nock

---
+++ ibgeService.js
@@ -0,0 +1,5 @@
+module.exports.getUFs = async () => {
+    const axios = require('axios')
+    const result = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    return result.data
+}
